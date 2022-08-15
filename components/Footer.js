1a707ca77ce4f72feb40872d754bee19c8099ef3
Waleed <waleedghori4@gmail.com>
2022-08-15 10:19:31 +0500

    start after 14 aug

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div>
-      <div className="bg-slate-500"> this is working fine</div>
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
-              <span className="ml-3 text-xl">Tailblocks</span>
+              <Link href={'/'}> 
+              <Image src="/codeswear.webp" alt=""  width={200} height={40}/>
+              </Link>
             </a>
-            <p className="mt-2 text-sm text-gray-500">
+            <p className="mt-2 text-sm text-gray-500 px-4">
               Air plant banjo lyft occupy retro adaptogen indego
             </p>
           </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+
 const Navbar = () => {
-  return <div className="bg-slate-900">Navbar</div>;
+  return <div className="flex flex-col md:flex-row md:justify-start justify-center items-center">
+    <div className="logo mx-4">
+      <Image src='/codeswear.webp' alt='Codeswear' width={250} height={45}/>
+    </div>
+    <div className="nav">
+      <ul className="flex item-center md:space-x-2 space-x-3 font-bold md:text-xl">
+        <Link href={'/'}><a><li>T-Shirts</li></a></Link>
+        <Link href={'/'}><a><li>Hoddies</li></a></Link>
+        <Link href={'/'}><a><li>Stickers</li></a></Link>
+        <Link href={'/'}><a><li>Mugs</li></a></Link>
+      </ul>
+    </div>
+    <div className="cart absolute right-0 top-2 mx-4">
+      <button>Cart</button>
+    </div>
+  </div>;
 };
 export default Navbar;
